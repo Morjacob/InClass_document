326380907c11db17ef503341722dee1c5395ed7a
Morgan Jacob <jacobmmori@gmail.com>
2024-12-02 17:36:46 -0500

    customization

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ new Chart(
                     label:"Votes",
                     data:[12,19,3],
                     borderWidth: 4,
-                    backgroundColor: ['red','blue','yellow'],
+                    backgroundColor: [ 
+                        'rgba(245,0,9,0.45)',
+                        'rgba(5,0,249,0.45)',
+                        'rgba(250,250,5,0.45)'],
                     borderColor: [
                         'rgba(245,0,9)',
-                        'rgba(5,0,249',
+                        'rgba(5,0,249)',
                         'rgba(250,250,5)'
                     ]
                 }
